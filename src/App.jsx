80181bb9a599aff3acb9fbea9340ead518c5a09e
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-09-04 23:26:13 -0700

    Updated handleRemovePlace function to include updateUserPlaces hook which filters the userPlaces to keep only the ones that don't match their ID. Placed code inside try/catch with same rever to old State if error plus re-setting errorUpdatingPlaces state accordingly.

--- src/App.jsx
+++ src/App.jsx
@@ -54,8 +54,18 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
+    try {
+      await updateUserPlaces(
+        userPlaces.filter(place => place.id !== selectedPlace.current.id)
+      );
+    } catch (error) {
+      setUserPlaces(userPlaces); // if an error occurs, revert to the previous state.
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to delete place.' });
+    }
+
+
     setModalIsOpen(false);
-  }, []);
+  }, [userPlaces]);
 
   function handleError() {
     setErrorUpdatingPlaces(null);
