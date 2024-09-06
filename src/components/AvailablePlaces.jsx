dc7e385a1226b2e5a534f00f1b8240a334ad03fc
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-09-05 22:54:36 -0700

    Created new fetchSortedPlaces function which indirectly calls fetchAvailablePlaces asynchronously and returns a Promise which is expected by fetchFunction within the useFetch definition. Updated the useFetch function call to call the new fetchSotredPlaces function instead.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,21 +4,29 @@ import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import { useFetch } from '../hooks/useFetch.js';
 
-export default function AvailablePlaces({ onSelectPlace }) {
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
 
-  // navigator.geolocation.getCurrentPosition(
-  //   (position) => {
-  //     const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-  //     setAvailablePlaces(sortedPlaces);
-  //     setIsFetching(false);
-  //   });
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      resolve(sortedPlaces);
+    });
+  });
+}
+
+export default function AvailablePlaces({ onSelectPlace }) {
 
   const {
     isFetching,
     error,
     fetchedData: availablePlaces,
-    setFetchedData: setAvailablePlaces,
-  } = useFetch(fetchAvailablePlaces, []);
+  } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <ErrorMsg title="An Error Occurred!" message={error.message} onConfirm={() => setError(null)} />;
