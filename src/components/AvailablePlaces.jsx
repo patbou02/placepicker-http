65e5fd0affaa6f0f6e38ff0ba1a4ad6662a243fd
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-09-05 22:43:46 -0700

    Refactored AvailablePlaces component to use the useFetch custom hook. Removed uneeded State as well as useEffect usage. Extracted custom location related code and commented it out for later tweaks.

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,36 +1,24 @@
-import { useState, useEffect } from 'react';
-
 import ErrorMsg from './Error.jsx';
 import Places from './Places.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);        // LOADING State
-  const [availablePlaces, setAvailablePlaces] = useState([]); // DATA State
-  const [error, setError] = useState(null);                   // ERROR State
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
+  // navigator.geolocation.getCurrentPosition(
+  //   (position) => {
+  //     const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
+  //     setAvailablePlaces(sortedPlaces);
+  //     setIsFetching(false);
+  //   });
 
-      try {
-        const places = await fetchAvailablePlaces();
-
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-          const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({ message: error.message || 'An unknown error occurred!' });
-        setIsFetching(false);
-      }
-    }
-    
-    fetchPlaces();
-  }, []);
+  const {
+    isFetching,
+    error,
+    fetchedData: availablePlaces,
+    setFetchedData: setAvailablePlaces,
+  } = useFetch(fetchAvailablePlaces, []);
 
   if (error) {
     return <ErrorMsg title="An Error Occurred!" message={error.message} onConfirm={() => setError(null)} />;
